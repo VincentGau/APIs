e1b4cbdc462f53dbb6db7d621817dbd6ee5c4858
Haku-Home <gmkohaku@hotmail.com>
2019-04-07 11:57:11 +0800

    add mvc app

---
+++ MVCApp/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using MVCApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Json(new { aaa = "bbb", user = new User("Ming", "Gao") }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ MVCApp/Models/user.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCApp.Models
+{
+    public class User
+    {
+        public User(string firstname, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+    }
+}
